d5c41f0d34b9a2d3d24a5bcb91ec768b54d0c60c
Tim Guo <timguoqk@users.noreply.github.com>
2016-12-04 20:45:19 -0500

    ctt for books

--- preprocessing/convert_to_tfrecord.py
+++ preprocessing/convert_to_tfrecord.py
@@ -29,16 +29,30 @@ def convert_clozes():
 
 
 def convert_books():
-    # filenames = [f for f in os.listdir(BOOKS_DIR)
-    #              if os.path.isfile(BOOKS_DIR + f)]
-    # res = ''
-    # for fname in filenames:
-    #     with open(BOOKS_DIR + fname) as f:
-    #         res += proces_text(f.read()) + ' STOP '
-    # vec_res = [vocab.get(word, vocab['UNK']) for word in res.split()]
-    # with open('books_training', 'wb') as f:
-    #     pickle.dump(vec_res, f, 2)
-    pass
+    def _list64(l):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=l))
+    BLANK = vocab['BLANK']
+    STOP = vocab['BLANK']
+
+    with open('more_generated_clozes', 'rb') as f:
+        clozes = pickle.load(f)
+    writer = tf.python_io.TFRecordWriter('./books.tfrecords')
+    for article in clozes:
+        blank_loc = [i for i, x in enumerate(article['text_v']) if x == BLANK]
+        print(len(blank_loc))
+        for num_b, b in enumerate(blank_loc):
+            stop_prev = next(i for i in range(b - 1, -2, -1)
+                             if i == -1 or article['text_v'][i] == STOP)
+            stop_next = next(i for i in range(b + 1, len(article['text_v']) + 1)
+                             if i == len(article['text_v']) or article['text_v'][i] == STOP)
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'document': _list64(article['text_v']),
+                'query': _list64(article['text_v'][stop_prev:stop_next]),
+                'answer': _list64([article['keys_v'][num_b]]),
+                'choices': _list64(article['choices_v'][num_b])
+            }))
+            writer.write(example.SerializeToString())
+    writer.close()
 
 
 if __name__ == '__main__':
@@ -46,4 +60,4 @@ if __name__ == '__main__':
         vocab = pickle.load(f)
 
     convert_clozes()
-    # convert_books()
+    convert_books()
